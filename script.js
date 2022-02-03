154a18c0647f8b30ffdbdf2c3614e4849cb41ec4
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-03 17:00:59 -0300

    requisito 3: ok

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
